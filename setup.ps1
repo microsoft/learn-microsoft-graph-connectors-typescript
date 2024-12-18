005d2508e228b48c984670e09144372de2d96317
Waldek Mastykarz <waldek@mastykarz.nl>
2024-12-18 18:42:24 +0100

    Fixes

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,13 @@
 # login
 Write-Host "Sign in to Microsoft 365..."
-npx -p @pnp/cli-microsoft365 -- m365 login --authType browser
+npx -p @pnp/cli-microsoft365 -- m365 login --authType browser --appId 14d82eec-204b-4c2f-b7e8-296a70dab67e
+
+Start-Process "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=14d82eec-204b-4c2f-b7e8-296a70dab67e&response_type=code&scope=https%3A%2F%2Fgraph.microsoft.com%2FApplication.ReadWrite.All%20https%3A%2F%2Fgraph.microsoft.com%2FAppRoleAssignment.ReadWrite.All"
+
+Read-Host -Prompt "Press Enter to continue when ready"
+
+Write-Output "Waiting 10s for permissions to propagate..."
+Start-Sleep -Seconds 10
 
 # create AAD app
 Write-Host "Creating AAD app..."
--- setup.sh
+++ setup.sh
@@ -2,7 +2,14 @@
 
 # login
 echo "Sign in to Microsoft 365..."
-npx -p @pnp/cli-microsoft365 -- m365 login --authType browser
+npx -p @pnp/cli-microsoft365 -- m365 login --authType browser --appId 14d82eec-204b-4c2f-b7e8-296a70dab67e
+
+open "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=14d82eec-204b-4c2f-b7e8-296a70dab67e&response_type=code&scope=https%3A%2F%2Fgraph.microsoft.com%2FApplication.ReadWrite.All%20https%3A%2F%2Fgraph.microsoft.com%2FAppRoleAssignment.ReadWrite.All"
+
+read -p "Press enter to continue when ready"
+
+echo "Waiting 10s for permissions to propagate..."
+sleep 10
 
 # create Entra app
 echo "Creating Entra app..."
